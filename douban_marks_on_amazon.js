461d76a777632f248c5fc052a09e1eba536c94ff
udonmai <udonmai@gmail.com>
2015-01-29 14:10:20 +0900

    update

--- douban_marks_on_amazon.js
+++ douban_marks_on_amazon.js
@@ -1,24 +1,25 @@
 // ==UserScript==
-// @name                douban_marks_on_amazon_new
-// @namespace           douban_marks_on_amazon_new
-// @version             0.5
-// @include             http://www.amazon.cn/*
-// author               udonmai@gmail.com
-// 2012-02-22           inition
-// 2012-02-25           Thanks to wong2
-// 2012-02-26                   Taking into use
-// 2012-09-16                   update with the amazon.cn's update
-// 2013-10-08                   support the ebook pages on anmazon.cn
+// @name        douban_marks_on_amazon
+// @namespace   douban_marks_on_amazon
+// @version     0.5
+// @include     *//www.amazon.cn/*
+// author       udonmai@gmail.com
+// 2012-02-22   inition
+// 2012-02-25   Thanks to wong2
+// 2012-02-26   Taking into use
+// 2012-09-16   update with the amazon.cn's update
+// 2013-10-08   support the ebook pages on anmazon.cn
+// 2015-01-29   update
 // ==/UserScript==
 
-var $ = function (selector) {
+var $ = function(selector){
     return document.querySelectorAll(selector);
 };
 
-!function () {
+!function(){
     //var nav = $('.nav-category-button a').text;
     //if(nav !== "图书") {
-    //      return;
+    //  return;
     //}
 
     var isbn = "";
@@ -31,23 +32,26 @@ var $ = function (selector) {
         if (info.textContent == "ISBN:" || info.textContent == "条形码:") {
             isbn = info.nextSibling.data;
             isbn = isbn.split(",")[0].substring(1);
+            console.log(isbn);
             break;
         }
     }
-
+    
     var xht2 = function(pnum){
         GM_xmlhttpRequest({
             method: 'GET',
             url:    'http://api.douban.com/book/subject/isbn/' + isbn + '?alt=json',
             onload: function(res) {
                 var rejson = JSON.parse(res.responseText);
-
+    
                 var numRaters = rejson['gd:rating']['@numRaters'];
                 var average = rejson['gd:rating']['@average'];
                 var link = rejson['link'][1]['@href'];
-                var emp = $('.buying')[pnum];
-
+                //var emp = $('.buying')[pnum];
+                var emp = $('#productGuarantee_feature_div')[0];
+                console.log(emp);
                 var pos;
+    
                 if(average < 1) pos = -151;
                 else if(average >=1 && average < 2) pos = -136;
                 else if(average >=2 && average < 3) pos = -121;
@@ -58,21 +62,22 @@ var $ = function (selector) {
                 else if(average >=7 && average < 8) pos = -46;
                 else if(average >=8 && average < 9) pos = -31;
                 else if(average >=9 && average < 10) pos = -16;
-                else if(average == 10) pos = -1;
-
+                else if(average = 10) pos = -1;
+    
                 var htmlstr = document.createElement("span");
-                htmlstr.innerHTML =  "&nbsp|&nbsp<span style='color:#0C7823; font-weight:700;'>豆瓣</span>评分:<span style='background-image:url(http://img3.douban.com/pics/all_bigstars.gif); background-repeat:no-repeat; background-position:0 "+ pos +"; width:75px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>";
-
+                htmlstr.innerHTML =  "&nbsp|&nbsp<span style='color:#0C7823; font-weight:700;'>豆瓣</span>评分:<span style='background-image:url(http://img3.douban.com/pics/all_bigstars.gif); background-repeat:no-repeat; background-position:0 "+ pos +"px; width:75px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>";
+    
                 if(numRaters < 10) htmlstr.innerHTML += "<span>&nbsp少于10人评价&nbsp<a href='"+ link +"'>链接</a></span>";
                 else htmlstr.innerHTML += "<span>"+ average +"&nbsp("+ numRaters +"人评价)&nbsp<a href='"+ link +"'>链接</a></span>";
+                console.log(htmlstr);
                 emp.appendChild(htmlstr);
             }
         });
     };
-
+    
     //若找不到ISBN，初步认为此页是电子书页面
-    if(isbn == '') {
-
+    if(isbn === '') {
+        
         var olinks = $(".tmm_bookTitle a");
         for (var i = 0; i <= 2; i++) {
             olink = olinks[i];
@@ -93,7 +98,7 @@ var $ = function (selector) {
                 //ajax返回的HTML文档实为string类型
                 var el = document.createElement('div');
                 el.innerHTML = res.responseText;
-
+                
                 var isbns = el.querySelectorAll("div.content b");
                 for (var i = 0; i <= 10; i++) {
                     var info = isbns[i];
@@ -103,13 +108,13 @@ var $ = function (selector) {
                         break;
                     }
                 }
-
+                
                 xht2(3);//电子书
                 return;
             }
         });
     }
-
+  
     xht2(2);//纸书
 
 }();
